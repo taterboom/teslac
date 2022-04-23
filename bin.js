9be4dc1b25112aae7fbce112ebb0aaf9fbccc38d
xueyong <1085523029@qq.com>
2022-04-23 13:43:34 +0800

    chore: rm test code

--- bin.js
+++ bin.js
@@ -6,8 +6,6 @@ var access_token = process.argv[2] || process.env.TESLA_ACCESS_TOKEN
 
 var refresh_token = process.argv[3] || process.env.TESLA_REFRESH_TOKEN
 
-console.log(process.env.TESLA_ACCESS_TOKEN, process.env.TESLA_REFRESH_TOKEN, process.argv[2])
-
 var tc = new TeslaClient(access_token, refresh_token)
 
 tc.init()
